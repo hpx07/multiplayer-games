ed806504d9b7ce5c5f56119807d3f6502bd9e988
hpx07 <yournamepleaseplease@gmail.com>
2026-02-17 17:39:41 +0530

    updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 // ============================================
 // SHARED STATE
